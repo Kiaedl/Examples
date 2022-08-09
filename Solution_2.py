a0b494bbac1ff8541d354b510eb2088575ab4eeb
Kiaedl <dmitryloskutov00@gmail.com>
2022-08-09 12:14:14 +0300

    Python_example_2

---
+++ Solution_2.py
@@ -0,0 +1,10 @@
+﻿s = [int(input()) for i in range((int(input()))+1)]
+n = "НЕТ"
+for j in range(len(s)-1):
+    if n != "НЕТ":
+        break
+    for k in range(j+1, len(s)-1):
+        if s[j] * s[k] == s[-1]:
+            n = "ДА"
+            break
+print(n)
