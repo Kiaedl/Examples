6b796c70739cca1692514dc3344d8bd99369dfa6
Kiaedl <dmitryloskutov00@gmail.com>
2022-08-09 13:32:59 +0300

    Example_SQL_2

---
+++ Example_SQL2.sql
@@ -0,0 +1,8 @@
+﻿UPDATE fine,(
+    SELECT name, number_plate, violation
+    FROM fine
+    GROUP BY  1, 2, 3
+    HAVING COUNT(3)>1
+    ORDER BY 1,2,3) AS new
+SET sum_fine = IF(date_payment is NULL,sum_fine*2,sum_fine)
+WHERE fine.name = new.name;
