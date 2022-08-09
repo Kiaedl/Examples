eb47fecf856d5221d46f2db0a776587935b0487b
Kiaedl <dmitryloskutov00@gmail.com>
2022-08-09 13:19:28 +0300

    SQL_Example

---
+++ SQL_solution.sql
@@ -0,0 +1,14 @@
+﻿SELECT
+   Department,
+   Employee,
+   Salary
+FROM(SELECT
+        dense_rank() OVER (partition BY DepartmentId ORDER BY Salary DESC) AS rang,
+        Employee.Name AS Employee,
+        Salary,
+        Department.Name AS Department
+     FROM Department INNER JOIN Employee
+     ON Employee.DepartmentId=Department.Id)as top
+WHERE rang<=3
+ORDER BY Department, rang
+;
